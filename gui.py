07bb87f7db9d86b59844432e4ca330e3fa083f96
Leo Huang <klhftco@gmail.com>
2022-10-23 12:42:12 -0700

    finish class reconstructino

--- gui.py
+++ gui.py
@@ -23,10 +23,10 @@ class App(tk.Tk):
         self.attributes('-fullscreen', True)
 
         # location - pack(), grid(), place()
-        self.lbl = tk.Label(root, text="Are you ready?", font=genFont)
+        self.lbl = tk.Label(self, text="Are you ready?", font=genFont)
         self.lbl.place(relx = 0.5, rely=0.8, anchor=tk.CENTER)
 
-        self.btn = tk.Button(root, text="Start", command=start, \
+        self.btn = tk.Button(self, text="Start", command=self.start, \
                      font=genFont, width=25, height=2, bg='#000', fg='#00FF00')
         self.btn.place(relx=0.5, rely=0.85, anchor=tk.CENTER)
 
@@ -35,26 +35,27 @@ class App(tk.Tk):
         self.max_x = screen_width - 50
         self.max_y = screen_height - 50
 
+        self.canvas = tk.Canvas(self, height=screen_height, width=screen_width, bg="#EEE")
+
     def start(self):
         self.lbl.destroy()
         self.btn.destroy()
 
-        canvas = tk.Canvas(root, height=screen_height, width=screen_width, bg="#EEE")
-
         # dot initialization
-        x1, y1, x2, y2 = min_x - dot_r, min_y - dot_r, min_x + dot_r, min_y + dot_r
-        self.dot = canvas.create_oval(x1, y1, x2, y2, outline='#EEE', fill="black", state=tk.HIDDEN)
+        x1, y1, x2, y2 = self.min_x - dot_r, self.min_y - dot_r, self.min_x + dot_r, self.min_y + dot_r
+        self.dot = self.canvas.create_oval(x1, y1, x2, y2, outline='#EEE', fill="black", state=tk.HIDDEN)
 
-        canvas.pack()
+        self.canvas.pack()
 
         # animation steps
-        canvas.after(1000, lambda: self.blink(self.dot, 1))
-        canvas.after(5000, lambda: canvas.itemconfig(o1, state=tk.NORMAL))
-        canvas.after(5000, lambda: self.move_dot(self.dot, 1))
+        self.canvas.after(1000, lambda: self.blink(self.dot, 1))
+        self.canvas.after(5000, lambda: self.canvas.itemconfig(self.dot, state=tk.NORMAL))
+        self.canvas.after(5000, lambda: self.move_dot(self.dot, 1))
 
     def move_dot(self, dot, stage):
+        print('moving')
         MOVE_TIMESTEP = 30
-        ox, oy = canvas.coords(dot)[0] + 10, canvas.coords(dot)[1] + 10
+        ox, oy = self.canvas.coords(dot)[0] + dot_r, self.canvas.coords(dot)[1] + dot_r
         if stage == 1:
             bound = oy >= self.max_y
             step_x = 0
@@ -73,36 +74,48 @@ class App(tk.Tk):
             step_y = 0
         elif stage == 5:
             bound = ox >= self.max_x
-            step_x = (self.max_x) - (self.min_x)) / 100
-            step_y = (self.max_y) - (self.min_y)) / 100
+            step_x = ((self.max_x) - (self.min_x)) / 100
+            step_y = ((self.max_y) - (self.min_y)) / 100
         elif stage == 6:
             bound = ox <= self.min_x
             step_x = -10
             step_y = 0
         elif stage == 7:
             bound = ox >= self.max_x
-            step_x = (self.max_x) - (self.min_x)) / 100
-            step_y = -(self.max_y) - (self.min_y)) / 100
+            step_x = ((self.max_x) - (self.min_x)) / 100
+            step_y = -((self.max_y) - (self.min_y)) / 100
         elif stage == 8:
             bound = ox <= self.min_x
             step_x = -10
             step_y = 0
         elif stage == 9:
-            root.destroy()
+            self.destroy()
             return
 
         if bound:
             self.move_dot(dot, stage + 1)
         else:
-            canvas.move(dot, step_x, step_y)
-            canvas.after(MOVE_TIMESTEP, lambda: self.move_dot(dot, stage))
+            self.canvas.move(dot, step_x, step_y)
+            self.canvas.after(MOVE_TIMESTEP, lambda: self.move_dot(dot, stage))
 
     def blink(self, dot, stage):
+        print('blinking')
+        TIMESTEP_BLINK = 1000
         if stage == 1:
-            canvas.move(dot, min_x, min_y)
+            pass
+        if stage == 2:
+            self.canvas.move(dot, 0, (self.max_y - self.min_y))
+        if stage == 3:
+            self.canvas.move(dot, (self.max_x - self.min_x), 0)
+        if stage == 4:
+            self.canvas.move(dot, 0, -(self.max_y - self.min_y))
+        if stage == 5:
+            self.canvas.move(dot, -(self.max_x - self.min_x), 0)
+            return
 
-        canvas.itemconfig(dot, state=tk.NORMAL)
-        canvas.after(1000, lambda: canvas.itemconfig(dot, state=tk.HIDDEN))
+        self.canvas.itemconfig(dot, state=tk.NORMAL)
+        # self.canvas.after(TIMESTEP_BLINK - 1, lambda: self.canvas.itemconfig(dot, state=tk.HIDDEN))
+        self.canvas.after(TIMESTEP_BLINK, lambda: self.blink(dot, stage + 1))
 
 # execute tkinter if run
 if __name__ == "__main__":
